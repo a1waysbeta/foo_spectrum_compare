30e5e406ee8fdbfcb1178c01762cf3c9ee41b25d
a1waysbeta <2366650060@qq.com>
2026-08-21 17:06:15 +0800

    Add files via upload

--- src/stdafx.h
+++ src/stdafx.h
@@ -1,6 +1,11 @@
 #pragma once
 
 #ifdef __cplusplus
+// winsock2.h must be included BEFORE anything that pulls in winsock.h
+#include <winsock2.h>
+// Prevent the old winsock.h from being included (avoids redefinition conflicts)
+#define _WINSOCKAPI_
+#define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <windows.h>
 #include <windowsx.h>
